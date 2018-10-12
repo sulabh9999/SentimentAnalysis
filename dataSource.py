a05c68aa9bf1b47fb489fa73c14aafb03cd4a4ad
sulabh <sulabh9999@gmail.com>
2018-10-12 23:17:19 +0530

    Bar graph added

--- dataSource.py
+++ dataSource.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def getData(input_file_path):
     input_file = input_file_path
-    df = pd.read_csv(input_file, sep='\t', usecols=['Name', 'Comment'])
-    return df['Comment'].values
+    #print('---input_file_path--', input_file_path)
+    df = pd.read_csv(input_file, sep='\t', usecols=['comments', 'ratings', 'dates'])
+    return df
 
