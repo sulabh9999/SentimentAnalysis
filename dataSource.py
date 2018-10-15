2caee3cfe8b8e4a5ad531370d51df4d426c92937
sulabh <sulabh9999@gmail.com>
2018-10-15 21:46:06 +0530

    Filtered csv rows by given dates

--- dataSource.py
+++ dataSource.py
@@ -1,9 +1,19 @@
 import pandas as pd
+from datetime import datetime
 
+# return dataframe between given dates
+def getData(file_Path, start_date=None, end_date=None):
+    df = pd.read_csv(file_Path, sep='\t', usecols=['comments', 'ratings', 'dates']) 
 
-def getData(input_file_path):
-    input_file = input_file_path
-    #print('---input_file_path--', input_file_path)
-    df = pd.read_csv(input_file, sep='\t', usecols=['comments', 'ratings', 'dates'])
+    if (start_date and end_date) is not None:
+        df['formatedDate']=pd.to_datetime(df['dates'])
+#         print('...datw..', df['formatedDate'])
+        df.sort_values(by=['formatedDate'], ascending=True, inplace=True)
+        dateField = df['formatedDate']
+        start = datetime.strptime(start_date, '%d-%m-%Y') 
+        end = datetime.strptime(end_date, '%d-%m-%Y')
+        df = df.loc[(dateField>=start) & (dateField<=end)]
     return df
 
+
+    
