b6e0c7247c2cd76f328cb5a1e0a465abfa5806be
sulabh <sulabh9999@gmail.com>
2018-12-09 22:14:59 +0530

    pi chart adding

--- showBarGraph.py
+++ showBarGraph.py
@@ -6,15 +6,19 @@ def showBarCharForSentiment(topicWithPotential, pos=True):
     # get average percentage of sentiment for each unique word in all comments
 #     allTopics = {k: v[0]/v[1] for k, v in topicWithPotential.items()}
 #     from operator import itemgetter
-    dataMap = sorted(topicWithPotential, key=lambda kv: kv[1][1]/kv[1][2],  reverse=True)
+#     dataMap = sorted(topicWithPotential, key=lambda kv: kv[1][1]/kv[1][2],  reverse=True)
 #     print(dataMap)
     # this is for plotting purpose
     labels = []
     polarity = []
   
-    for each in dataMap[: 20]:
+    for each in topicWithPotential[: 20]:
         labels.append(each[0])
-        polarity.append(each[1][1]/each[1][2])
+        if pos:
+            polarity.append(each[1][1])
+        else:
+            polarity.append(each[1][0])
+        
 
     plt.figure(figsize=(20, 10)) 
     x = np.arange(len(polarity))
@@ -28,3 +32,19 @@ def showBarCharForSentiment(topicWithPotential, pos=True):
     plt.ylabel('Polarity', fontsize=15)
     plt.xticks(x, labels, fontsize=12, rotation=70)
     plt.title('People opinion most -ve on digiBank app store')
+
+def showPiChart(sortedDict):
+    pieLabels = []
+    populationShare = []
+    localDict = sortedDict[:10]
+    for each in localDict:
+        pieLabels.append(each[0])
+        populationShare.append(each[1][2])
+        
+    figureObject, axesObject = plt.subplots()
+    axesObject.pie(populationShare,
+            labels=pieLabels,
+            autopct='%1.2f',
+            startangle=90)
+    axesObject.axis('equal')
+    plt.show()
