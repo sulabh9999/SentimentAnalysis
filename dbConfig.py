a8a29b63ce21b9f3de69e1a9d0878b2fe4883203
sulabh <sulabh9999@gmail.com>
2019-02-24 23:28:12 +0530

    pkl file added

--- dbConfig.py
+++ dbConfig.py
@@ -2,25 +2,26 @@
 
 
 import pymysql
+import re
     
 class dbSQL:
     
     def __init__(self):        
-        self.connection = pymysql.connect(host= "localhost", user="root", passwd="1234", db='DGIndia')
+        self.connection = pymysql.connect(host= "localhost", user="root", passwd="1234", db='DGIndia', charset='utf8')
         self.curs = self.connection.cursor()
+        self.curs.execute('SET collation_connection = utf8mb4_bin')
 
-    def insert(self, data):
-        escaped = data[2].translate(str.maketrans({"-":  r"\-",
-                                          "]":  r"\]",
-                                          "\\": r"\\",
-                                          "^":  r"\^",
-                                          "$":  r"\$",
-                                          "*":  r"\*",
-                                          ".":  r"\."}))
+    def insert(self, rawData):
+        name = self.addEsapesequence(rawData[0])
+        date = '2019-02-10'
+        comment = self.addEsapesequence(rawData[2])
+        rating = rawData[3]
         
-        qry = """INSERT INTO `android_full_comments`  VALUES (NULL,'%s','%s','%s','%s')"""
+        data = (name, date, comment, rating)
+        qry = """INSERT INTO `android_full_comments`  VALUES (NULL, '%s','%s','%s','%s')"""
         #data = ('sulabh shukla', '2019-02-05', 'nice work ...', '3')
         #query = """INSERT INTO `android_full_comments`  VALUES (NULL, 'sulabh shukla', '2019-02-05', 'nice work ...', '3');"""
+        print('query:', qry % data)
         self.__execute(qry % data)
     
     def show(self):
@@ -41,3 +42,6 @@ class dbSQL:
             self.connection.rollback()
         self.connection.close()
         
+    def addEsapesequence(self, comment): 
+#         return re.sub("([#$%^&_{}'\"])", r"\\\1", comment)
+        return re.sub("(['\"])", r"\\\1", comment)
